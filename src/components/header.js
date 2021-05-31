89b37cc1f189a2a6a8064fda5f4e70fae01618f1
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-05-30 20:01:34 -0400

    Minor changes in Header

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { Grid, Switch, Container } from '@material-ui/core';
+import { Grid, Switch, Container, Typography } from '@material-ui/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
 import './style.css';
@@ -9,38 +9,35 @@ class Header extends Component {
         console.log("Clicked", event.target.checked)    //returns true and false
     };
     render() {
-        return (
-            <Fragment>
-                <Container maxWidth="xl" className="headerContainer">
-                    <Container maxWidth="md" className="headeritems">
-
-                        <Grid container className="headerContainer" alignItems="center" spacing={1} justify="space-between" >
-                            <Grid item className="logo">
-                                DevJobs
+        return <>
+            <Container maxWidth="xl" className="headerContainer">
+                <Container maxWidth="lg" className="headeritems">
+                    <Grid container className="headerContainer" alignItems="center" spacing={1} justify="space-between" >
+                        <Grid item className="logo">
+                            <Typography variant="h5">devJobs</Typography>
                         </Grid>
-                            <Grid item className="button">
-                                <Grid component="label" container alignItems="center" spacing={1}>
-                                    <Grid item>
-                                        <FontAwesomeIcon className="icon" icon={faSun} />
-                                    </Grid>
-                                    <Grid item>
-                                        <Switch
-                                            color="default"
-                                            onChange={this.handleChange}
-                                            inputProps={{ 'aria-label': 'primary checkbox' }} />
-                                    </Grid>
-                                    <Grid item>
-                                        <FontAwesomeIcon className="icon" icon={faMoon} />
+                        <Grid item className="button">
+                            <Grid component="label" container alignItems="center" spacing={1}>
+                                <Grid item>
+                                    <FontAwesomeIcon className="icon" icon={faSun} />
+                                </Grid>
+                                <Grid item>
+                                    <Switch
+                                        color="default"
+                                        onChange={this.handleChange}
+                                        inputProps={{ 'aria-label': 'primary checkbox' }} />
+                                </Grid>
+                                <Grid item>
+                                    <FontAwesomeIcon className="icon" icon={faMoon} />
 
-                                    </Grid>
                                 </Grid>
                             </Grid>
                         </Grid>
-                    </Container>
-
+                    </Grid>
                 </Container>
-            </Fragment>
-        );
+            </Container>
+        </>
+
     }
 }
 
