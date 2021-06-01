1edfa52d4ec5d582eb5c6b95d7742a49beed20a0
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-06-01 01:10:10 -0400

    improved responsiveness

--- src/components/card.js
+++ src/components/card.js
@@ -1,40 +1,57 @@
 import React, { Component, Fragment } from 'react';
 import { Card, CardActionArea, CardContent, CardActions, Typography, CardMedia, Container, Tooltip } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { ThemeProvider, muiThemeable, useTheme } from '@material-ui/styles';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
 import './style.css';
-const image = 'https://media-exp1.licdn.com/dms/image/C4D0BAQHiNSL4Or29cg/company-logo_200_200/0/1519856215226?e=2159024400&v=beta&t=r--a5-Dl4gvVE-xIkq8QyBzZ8mQ-OYwBOrixNzR95H0';
-const image1 = 'https://cdn.icon-icons.com/icons2/2429/PNG/512/microsoft_logo_icon_147261.png';
 
 
-const useStyles = makeStyles({
-    cardRoot: {
-        marginTop: 40,
-        overflow: "visible",     //To make Logo Visible outside of card
-        padding: 15,
-        borderRadius: 5,
-    },
-    image: {
-        width: 100
-    },
-    customToolTip: {
-        fontSize: 18
-    },
-    cardContent: {
-        marginTop: 10
-    }
-});
+
+
 
 const JobCard = (props) => {
-    const { data: { type, title, company, location } } = props;
+
+    /* const theme = createMuiTheme({
+        palette: {
+            primary: {
+                // Purple and green play nicely together.
+                main: '#ff4400',
+            },
+            secondary: {
+                // This is green.A700 as hex.
+                main: '#11cb5f',
+            },
+        },
+    }); */
+
+    const useStyles = makeStyles({
+        cardRoot: {
+            marginTop: 40,
+            overflow: "visible",     //To make Logo Visible outside of card
+            padding: 15,
+            borderRadius: 5,
+            // backgroundColor: props.muiTheme.palette.primary1Color,
+            // color: theme.palette.primary.contrastText
+        },
+        image: {
+            width: 100
+        },
+        customToolTip: {
+            fontSize: 18
+        },
+        cardContent: {
+            marginTop: 10
+        }
+    });
+
+    const { data: { type, title, company, location, company_logo } } = props;
     const classes = useStyles();
     return <>
         <Card className={classes.cardRoot}>
             <CardActionArea>
-                <div className='imgContainer'>
-                    <img className='companyLogo' src={image1} />
+                <div className='imgContainer imageLogo'>
+                    <img className='companyLogo' src={company_logo} />
                 </div>
                 <CardContent className={classes.cardContent}>
-
                     <Typography variant='body2' color='textSecondary' component='p'>
                         1w ago | {type}
                     </Typography>
@@ -43,7 +60,6 @@ const JobCard = (props) => {
                             {title}
                         </Typography>
                     </Tooltip>
-
                     <Typography variant='body2' color='textSecondary' component='p'>
                         {company}
                     </Typography>
